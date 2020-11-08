2bf167ffcf2a76109d9d143bb57960f5921c461f
Metious <71298690+Metious@users.noreply.github.com>
2020-11-08 05:35:56 +0330

    version bump

---
+++ SeamothBrineResist/Patches/DamageSystem_IsAcidImmune_Patch.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+namespace SeamothBrineResist.Patches
+{
+    [HarmonyPatch(typeof(DamageSystem), nameof(DamageSystem.IsAcidImmune))]
+    public static class DamageSystem_IsAcidImmune_Patch
+    {
+        [HarmonyPrefix]
+        public static bool Prefix(ref GameObject go, ref bool __result)
+        {
+            TechType techType = CraftData.GetTechType(go);
+            var acidImmune = DamageSystem.acidImmune == null ? new List<TechType>() : DamageSystem.acidImmune.ToList();
+            if (!acidImmune.Contains(TechType.Seamoth))
+                acidImmune.Add(TechType.Seamoth);
+            if (techType != TechType.None)
+            {
+                for (int i = 0; i < DamageSystem.acidImmune.Length; i++)
+                {
+                    if (techType == DamageSystem.acidImmune[i])
+                    {
+                        __result = true;
+                        return false;
+                    }
+                    DamageSystem.acidImmune = acidImmune.ToArray();
+                }
+            }
+            __result = false;
+            return true;
+        }
+    }
+}
