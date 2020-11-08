a0be26f737f7ea86501d3446a8aacc61791d7ff2
Metious <71298690+Metious@users.noreply.github.com>
2020-11-08 05:41:05 +0330

    Version bump

---
+++ CoffeeSoundFix/Main.cs
@@ -0,0 +1,14 @@
+using QModManager.API.ModLoading;
+using SMLHelper.V2.Handlers;
+namespace CoffeeSoundFix
+{
+    [QModCore]
+    public static class Main
+    {
+        [QModPatch]
+        public static void Load()
+        {
+            CraftDataHandler.SetEatingSound(TechType.Coffee, "event:/player/drink");
+        }
+    }
+}
