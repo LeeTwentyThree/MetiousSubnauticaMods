727f692f3f88b797902a3e7733987107480243a0
Metious <71298690+Metious@users.noreply.github.com>
2020-11-19 02:43:18 +0330

    updooted

---
+++ RockGrubPickupable/Main.cs
@@ -0,0 +1,49 @@
+﻿using QModManager.API.ModLoading;
+using System.Reflection;
+using System.IO;
+using SMLHelper.V2.Handlers;
+using SMLHelper.V2.Utility;
+using Sprite = Atlas.Sprite;
+using HarmonyLib;
+//using SMLHelper.V2.Commands;
+namespace RockGrubPickupable
+{
+    [QModCore]
+    public static class Main
+    {
+        private static Assembly myAssembly = Assembly.GetExecutingAssembly();
+        private static string ModPath = Path.GetDirectoryName(myAssembly.Location);
+        internal static string AssetsFolder = Path.Combine(ModPath, "Assets");
+        [QModPatch]
+        public static void Load()
+        {
+            InventoryAndWaterParkSetup();
+            Harmony.CreateAndPatchAll(myAssembly, $"Metious_{myAssembly.GetName().Name}");
+            //ConsoleCommandsHandler.Main.RegisterConsoleCommand("metiousit", typeof(Main), nameof(MetiousIt));
+        }
+        #region Inventory and WaterPark Setup
+        private static void InventoryAndWaterParkSetup()
+        {
+            LanguageHandler.SetTechTypeName(TechType.Rockgrub, "RockGrub");
+            LanguageHandler.SetTechTypeTooltip(TechType.Rockgrub, "A small, luminescent scavenger");
+
+
+            Sprite rockgrub = ImageUtils.LoadSpriteFromFile(Path.Combine(AssetsFolder, "RockGrub.png"));
+            if (rockgrub != null)
+            {
+                SpriteHandler.RegisterSprite(TechType.Rockgrub, rockgrub);
+            }
+            CraftDataHandler.SetItemSize(TechType.Rockgrub, new Vector2int(1, 1));
+            WaterParkCreature.waterParkCreatureParameters[TechType.Rockgrub] = new WaterParkCreatureParameters(0.03f, 0.7f, 1f, 1f, true);
+            CraftData.IsAllowed(TechType.Rockgrub);
+            BioReactorHandler.SetBioReactorCharge(TechType.Rockgrub, 350f);
+        }
+        /*[ConsoleCommand("metiousit")]
+        public static void MetiousIt(string name)
+        {
+            if (name == "rockgrub")
+                CraftData.AddToInventory(TechType.Rockgrub, 1, false, true);
+        }*/
+        #endregion
+    }
+}
---
+++ RockGrubPickupable/Patches/Creature_Start_Patch.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using UnityEngine;
+namespace RockGrubPickupable.Patches
+{
+    [HarmonyPatch(typeof(Creature), nameof(Creature.Start))]
+    class Creature_Start_Patch
+    {
+        [HarmonyPostfix]
+        public static void Postfix(Creature __instance)
+        {
+            // totally origina... *cough* stolen from MrPurple's CustomCommands code
+            TechType techType = CraftData.GetTechType(__instance.gameObject);
+            if (techType == TechType.Rockgrub)
+            {
+                Pickupable pickupable = __instance.gameObject.AddComponent<Pickupable>();
+                pickupable.isPickupable = true;
+                //pickupable.version = 4;
+
+                __instance.gameObject.EnsureComponent<WaterParkCreature>().pickupable = pickupable;
+                AquariumFish aquariumFish = __instance.gameObject.EnsureComponent<AquariumFish>();
+                aquariumFish.model = __instance.gameObject;
+
+                //the piece of code below makes the game in a loop  that it continues cloning the clone forever so im commenting it out
+                /*GameObject gameObject = Object.Instantiate<GameObject>(aquariumFish.model, Vector3.zero, Quaternion.identity);
+                AnimateByVelocity animateByVelocity = __instance.gameObject.GetComponentInChildren<AnimateByVelocity>();
+                animateByVelocity.rootGameObject = gameObject;
+                animateByVelocity.animationMoveMaxSpeed = 0.5f;*/
+            }
+        }
+    }
+}
