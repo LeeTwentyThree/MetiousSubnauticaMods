bd2bfeb8a75aa4db33792044a557b552e44c8f08
Metious <71298690+Metious@users.noreply.github.com>
2020-12-15 13:43:56 +0330

    Added Passive Sea Dragon mod

---
+++ PassiveSeaDragon/Patches/SeaDragon_Update_Patch.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+namespace PassiveSeaDragon.Patches
+{
+    [HarmonyPatch(typeof(SeaDragon), nameof(SeaDragon.Update))]
+    internal class SeaDragon_Update_Patch
+    {
+        [HarmonyPostfix]
+        public static void Postfix(SeaDragon __instance)
+        {
+            __instance.Aggression.Value = 0.0f;
+        }
+    }
+}
