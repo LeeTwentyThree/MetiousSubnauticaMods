9ebe256793b2e7395a9fbc2ee38de62fdd8e0f36
Metious <71298690+Metious@users.noreply.github.com>
2021-02-02 23:48:56 +0330

    Added BioChemical Batteries

---
+++ BioChemicalBatteries2/Main.cs
@@ -0,0 +1,77 @@
+﻿using QModManager.API.ModLoading;
+using SMLHelper.V2.Handlers;
+using SMLHelper.V2.Utility;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+using CustomBatteries.API;
+using BioChemicalBatteries2.Prefabs;
+namespace BioChemicalBatteries2
+{
+    [QModCore]
+    public static class Main
+    {
+        private static Assembly myAssembly = Assembly.GetExecutingAssembly();
+        private static string ModPath = Path.GetDirectoryName(myAssembly.Location);
+        internal static string AssetsFolder = Path.Combine(ModPath, "Assets");
+        internal static AssetBundle assetBundle = AssetBundle.LoadFromFile(Path.Combine(AssetsFolder, "biochemicalbatteries2"));
+        public const string version = "1.0.0.0";
+        [QModPatch]
+        public static void Load()
+        {
+            CreateAndPatchPacks();
+        }
+        private static void CreateAndPatchPacks()
+        {
+            var bioPlasma = new BioPlasma();
+            bioPlasma.Patch();
+
+            var bioChemBattery = new CbBattery()
+            {
+                ID = "BioChemBattery",
+                Name = "BioChemical Battery",
+                FlavorText = "Alterra Battery technology combined with a Warper power core makes for quite a potent renewable energy source.",
+                EnergyCapacity = 2500,
+                UnlocksWith = bioPlasma.TechType,
+                CraftingMaterials = new List<TechType>()
+                {
+                    bioPlasma.TechType,
+                    TechType.Magnetite, TechType.Magnetite
+                },
+                CustomIcon = ImageUtils.LoadSpriteFromFile(Path.Combine(AssetsFolder, "BioChemBattery.png")),
+                CBModelData = new CBModelData()
+                {
+                    UseIonModelsAsBase = true,
+                    CustomTexture = ImageUtils.LoadTextureFromFile(Path.Combine(AssetsFolder, "BioChemBatteryskin.png")),
+                    CustomSpecMap = ImageUtils.LoadTextureFromFile(Path.Combine(AssetsFolder, "BioChemBatteryspec.png")),
+                    CustomIllumMap = ImageUtils.LoadTextureFromFile(Path.Combine(AssetsFolder, "BioChemBatteryillum.png"))
+                },
+            };
+            bioChemBattery.Patch();
+
+            var bioChemCell = new CbBattery()
+            {
+                ID = "BioChemicalCell",
+                Name = "BioChemical Power Cell",
+                FlavorText = "Alterra Power Cell technology combined with a Warper power core makes for quite a potent renewable energy source.",
+                EnergyCapacity = bioChemBattery.EnergyCapacity * 2,
+                UnlocksWith = bioPlasma.TechType,
+                CraftingMaterials = new List<TechType>()
+                {
+                    bioChemBattery.TechType, bioChemBattery.TechType,
+                    TechType.Silicone
+                },
+                CustomIcon = ImageUtils.LoadSpriteFromFile(Path.Combine(AssetsFolder, "BioChemCell.png")),
+                CBModelData = new CBModelData()
+                {
+                    UseIonModelsAsBase = true,
+                    CustomTexture = ImageUtils.LoadTextureFromFile(Path.Combine(AssetsFolder, "BioChemCellskin.png")),
+                    CustomSpecMap = ImageUtils.LoadTextureFromFile(Path.Combine(AssetsFolder, "BioChemCellskin.png")),
+                    CustomIllumMap = ImageUtils.LoadTextureFromFile(Path.Combine(AssetsFolder, "BioChemCellillum.png"))
+                },
+            };
+            bioChemCell.Patch();
+        }
+    }
+}
---
+++ BioChemicalBatteries2/Prefabs/BioPlasma.cs
@@ -0,0 +1,57 @@
+﻿using SMLHelper.V2.Assets;
+using SMLHelper.V2.Handlers;
+using SMLHelper.V2.Utility;
+using UnityEngine;
+
+namespace BioChemicalBatteries2.Prefabs
+{
+    class BioPlasma : Spawnable
+    {
+        public BioPlasma()
+            : base("BioPlasma", "Bio Plasma Core", "A mysterious, powerful energy core harvested from a deceased Warper.")
+        {
+            OnFinishedPatching += () =>
+            {
+                CraftDataHandler.SetHarvestOutput(TechType.Warper, this.TechType);
+                CraftDataHandler.SetHarvestType(TechType.Warper, HarvestType.DamageDead);
+            };
+        }
+        protected override Atlas.Sprite GetItemSprite()
+        {
+            return SpriteManager.Get(TechType.Titanium);
+        }
+        public override GameObject GetGameObject()
+        {
+            GameObject prefab = Main.assetBundle.LoadAsset<GameObject>("BioPlasma.prefab");
+            GameObject obj = Object.Instantiate(prefab);
+
+            obj.EnsureComponent<TechTag>().type = this.TechType;
+            obj.EnsureComponent<PrefabIdentifier>().classId = this.ClassID;
+
+            Renderer[] renderers = obj.GetComponentsInChildren<Renderer>();
+            foreach(var rend in renderers)
+            {
+                foreach(var mat in rend.materials)
+                {
+                    mat.shader = Shader.Find("MarmosetUBER");
+                    mat.mainTexture = Main.assetBundle.LoadAsset<Texture2D>("warper_entrails");
+                    mat.SetTexture(ShaderPropertyID._SpecTex, Main.assetBundle.LoadAsset<Texture2D>("warper_entrails_spec"));
+                    mat.SetTexture(ShaderPropertyID._BumpMap, Main.assetBundle.LoadAsset<Texture2D>("warper_entrails_normal"));
+                }
+            }
+            SkyApplier skyApplier = obj.EnsureComponent<SkyApplier>();
+            skyApplier.anchorSky = Skies.Auto;
+            skyApplier.renderers = renderers;
+            skyApplier.enabled = true;
+
+            obj.EnsureComponent<Rigidbody>();
+
+            WorldForces wf = obj.EnsureComponent<WorldForces>();
+            wf.aboveWaterGravity = 0f;
+            wf.underwaterGravity = 0f;
+
+            prefab.SetActive(false);
+            return obj;
+        }
+    }
+}
