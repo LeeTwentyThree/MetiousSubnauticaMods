d0f73a63a936808554551d356d024997bbfbf325
Metious <71298690+Metious@users.noreply.github.com>
2021-02-12 11:54:41 +0330

    added drillablesulphur

---
+++ DrillableSulphur/Patches/CraftData_PreparePrefabIDCache_Patch.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+
+namespace DrillableSulphur.Patches
+{
+    class CraftData_PreparePrefabIDCache_Patch
+    {
+        [HarmonyPostfix]
+        public static void Postfix()
+        {
+            Dictionary<TechType, string> techMapping = CraftData.techMapping;
+            Dictionary<string, TechType> entClassTechTable = CraftData.entClassTechTable;
+
+            techMapping[TechType.DrillableSulphur] = "697beac5-e39a-4809-854d-9163da9f997e";
+            entClassTechTable["697beac5-e39a-4809-854d-9163da9f997e"] = TechType.DrillableSulphur;
+        }
+    }
+}
---
+++ DrillableSulphur/Patches/Drillable_Start_Patch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+namespace DrillableSulphur.Patches
+{
+    class Drillable_Start_Patch
+    {
+        [HarmonyPostfix]
+        public static void DrillableStart(Drillable __instance)
+        {
+            if (__instance.gameObject.name.Contains("DrillableSulphur"))
+            {
+                __instance.gameObject.EnsureComponent<TechTag>().type = TechType.DrillableSulphur;
+                __instance.gameObject.EnsureComponent<PrefabIdentifier>().ClassId = "697beac5-e39a-4809-854d-9163da9f997e";
+                __instance.gameObject.GetComponent<SkyApplier>().enabled = true;
+                ResourceTracker rt = __instance.gameObject.GetComponent<ResourceTracker>();
+                if (rt != null)
+                {
+                    rt.overrideTechType = TechType.Sulphur;
+                    rt.techType = TechType.Sulphur;
+                }
+            }
+        }
+    }
+}
