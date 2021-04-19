704696dd765e297fcc34eea8d650a3e219b5296b
Metious <71298690+Metious@users.noreply.github.com>
2021-04-19 10:21:01 +0430

    better null check

--- ExosuitPickupperPropulsionCannon/MonoBehaviours/PickupperPropulsionCannon.cs
+++ ExosuitPickupperPropulsionCannon/MonoBehaviours/PickupperPropulsionCannon.cs
@@ -9,6 +9,17 @@ namespace ExosuitPickupperPropulsionCannon.MonoBehaviours
         PropulsionCannon _propulsionCannon;
         Exosuit _exosuit;
 
+        Exosuit exosuit 
+        {
+            get 
+            {
+                if (_exosuit is null)
+                    return GetComponentInParent<Exosuit>();
+                    
+                return _exosuit;
+            }
+        }
+
         void Start()
         {
             _propulsionCannon = GetComponent<PropulsionCannon>();
@@ -37,14 +48,14 @@ namespace ExosuitPickupperPropulsionCannon.MonoBehaviours
             {
                 var pickupable = _activeTarget.GetComponent<Pickupable>();
 
-                if (pickupable is not null && pickupable.isPickupable && _exosuit.storageContainer.container.HasRoomFor(pickupable))
+                if (pickupable is not null && pickupable.isPickupable && exosuit.storageContainer.container.HasRoomFor(pickupable))
                 {
                     uGUI_IconNotifier.main.Play(pickupable.GetTechType(), uGUI_IconNotifier.AnimationType.From);
                     
                     pickupable = pickupable.Initialize();
 
                     var item = new InventoryItem(pickupable);
-                    _exosuit.storageContainer.container.UnsafeAdd(item);
+                    exosuit.storageContainer.container.UnsafeAdd(item);
                     pickupable.PlayPickupSound();
                 }
             }
