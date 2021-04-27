f2c8021108a2762cec2e642887ba249d7640742c
Metious <71298690+Metious@users.noreply.github.com>
2021-04-27 13:07:51 +0430

    fixed a bug for controller users

--- SeaMothAutomaticSonar/Main.cs
+++ SeaMothAutomaticSonar/Main.cs
@@ -9,12 +9,12 @@ namespace SeaMothAutomaticSonar
     {
         static readonly Assembly myAssembly = Assembly.GetExecutingAssembly();
 
-        public const string version = "1.0.0.0";
+        public const string version = "1.0.1.0";
 
         [QModPatch]
         public static void Load()
         {
-            CraftData.slotTypes[TechType.SeamothSonarModule] = QuickSlotType.Toggleable;
+            CraftData.slotTypes[TechType.SeamothSonarModule] = QuickSlotType.Selectable;
 
             Harmony.CreateAndPatchAll(myAssembly, $"Metious_{myAssembly.GetName().Name}");
         }
