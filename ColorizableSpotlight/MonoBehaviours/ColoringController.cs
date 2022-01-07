16e3c572f992260fb1d871dfa422454170f1e9c1
Metious <71298690+Metious@users.noreply.github.com>
2022-01-07 11:48:26 +0330

    Fixed NREs + performance improvements

--- ColorizableSpotlight/MonoBehaviours/ColoringController.cs
+++ ColorizableSpotlight/MonoBehaviours/ColoringController.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Globalization;
-using QModManager.Utility;
-using System.IO;
 using Logger = QModManager.Utility.Logger;
 using UnityEngine;
 #if SN1
@@ -19,61 +13,71 @@ namespace ColorizableSpotlight.MonoBehaviours
 {
     public class ColoringController : HandTarget, IHandTarget, IProtoEventListener
     {
-        Color savedColor = Color.white;
-        Light light;
-        Renderer[] renderers;
-        public void Start()
+        private Color _savedColor = Color.white;
+        private Light _light;
+        private Renderer[] _renderers;
+        private Renderer _particleRenderer;
+
+        private bool _dirty;
+        
+        private void Start()
         {
-            light = GetComponentInChildren<Light>(true);
-            renderers = GetComponentsInChildren<Renderer>();
+            _light = GetComponentInChildren<Light>(true);
+            _renderers = transform.GetChild(0).GetComponentsInChildren<Renderer>(true);
+            _particleRenderer = _renderers.First(r => r.material.shader.name.Contains("Particle"));
             
             PrefabIdentifier prefabIdentifier = GetComponentInParent<PrefabIdentifier>();
             if ((prefabIdentifier ??= GetComponent<PrefabIdentifier>()) is null)
                 return;
             
-            ColorInfo colorInfo;
             string saveFolder = Main.GetSaveFolderPath();
             string filePath = Main.Combine(saveFolder, prefabIdentifier.Id + ".json");
-            using (StreamReader reader = new StreamReader(filePath))
-            {
-                var serializer = new JsonSerializer();
-                colorInfo = serializer.Deserialize(reader, typeof(ColorInfo)) as ColorInfo;
-            }
             if (File.Exists(filePath))
             {
+                ColorInfo colorInfo;
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    var serializer = new JsonSerializer();
+                    colorInfo = serializer.Deserialize(reader, typeof(ColorInfo)) as ColorInfo;
+                }
+                
                 if (colorInfo != null)
                 {
-                    savedColor = new Color(colorInfo.RedLevel, colorInfo.GreenLevel, colorInfo.BlueLevel);
+                    _savedColor = new Color(colorInfo.RedLevel, colorInfo.GreenLevel, colorInfo.BlueLevel);
+                    _dirty = true;
                     return;
                 }
-                Logger.Log(Logger.Level.Warn,"olorInfo is null");
-                return;
+                Logger.Log(Logger.Level.Warn,"colorInfo is null");
             }
-            
-            Logger.Log(Logger.Level.Warn, "saved file doesn't exist");
         }
 
-        public void Update()
+        private void LateUpdate()
         {
-            light.color = new Color(savedColor.r, savedColor.g, savedColor.b);
-            foreach (var renderer in renderers)
+            if (_dirty)
             {
-                foreach (var material in renderer.materials)
+                _light.color = new Color(_savedColor.r, _savedColor.g, _savedColor.b);
+                foreach (var renderer in _renderers)
                 {
-                    material.SetColor(ShaderPropertyID._GlowColor, savedColor);
+                    foreach (var material in renderer.materials)
+                    {
+                        material.SetColor(ShaderPropertyID._GlowColor, _savedColor);
+                    }
+
+                    _particleRenderer.material.color = new Color(_savedColor.r, _savedColor.g, _savedColor.b, 0.45f);
                 }
+
+                _dirty = false;
             }
         }
 
-        public void OnHandClick(GUIHand hand)
-        {}
+        public void OnHandClick(GUIHand hand) {}
 
         public void OnHandHover(GUIHand hand)
         {
             if (!enabled)
                 return;
             
-            if (light == null)
+            if (_light == null)
             {
                 Logger.Log(Logger.Level.Error, "Light Component is missing!");
                 return;
@@ -84,42 +88,39 @@ namespace ColorizableSpotlight.MonoBehaviours
 
             if (Input.GetKeyDown(KeyCode.R))
             {
-                if (light.color.r >= 1.0f)
-                    light.color = new Color(0f, light.color.g, light.color.b);
+                if (_light.color.r >= 1.0f)
+                    _light.color = new Color(0f, _light.color.g, _light.color.b);
                 else
-                    light.color = new Color(light.color.r + 0.1f, light.color.g, light.color.b);
+                    _light.color = new Color(_light.color.r + 0.1f, _light.color.g, _light.color.b);
 
-                savedColor.r = light.color.r;
-                ErrorMessage.AddDebug($"Spotlight: Red levels updated ({light.color.r:0.00}/1)");
+                _savedColor.r = _light.color.r;
+                _dirty = true;
+                ErrorMessage.AddDebug($"Spotlight: Red levels updated ({_light.color.r:0.00}/1)");
             }
             else if (Input.GetKeyDown(KeyCode.G))
             {
-                if (light.color.g >= 1.0f)
-                    light.color = new Color(light.color.r, 0f, light.color.b);
+                if (_light.color.g >= 1.0f)
+                    _light.color = new Color(_light.color.r, 0f, _light.color.b);
                 else
-                    light.color = new Color(light.color.r, light.color.g + 0.1f, light.color.b);
+                    _light.color = new Color(_light.color.r, _light.color.g + 0.1f, _light.color.b);
 
-                savedColor.g = light.color.g;
-                ErrorMessage.AddDebug($"Spotlight: Green levels updated ({light.color.g:0.00}/1)");
+                _savedColor.g = _light.color.g;
+                _dirty = true;
+                ErrorMessage.AddDebug($"Spotlight: Green levels updated ({_light.color.g:0.00}/1)");
             }
             else if (Input.GetKeyDown(KeyCode.B))
             {
-                if (light.color.b >= 1.0f)
-                    light.color = new Color(light.color.r, light.color.g, 0f);
+                if (_light.color.b >= 1.0f)
+                    _light.color = new Color(_light.color.r, _light.color.g, 0f);
                 else
-                    light.color = new Color(light.color.r, light.color.g, light.color.b + 0.1f);
+                    _light.color = new Color(_light.color.r, _light.color.g, _light.color.b + 0.1f);
 
-                savedColor.b = light.color.b;
-                ErrorMessage.AddDebug($"Spotlight: Blue levels updated ({light.color.b:0.00}/1)");
+                _savedColor.b = _light.color.b;
+                _dirty = true;
+                ErrorMessage.AddDebug($"Spotlight: Blue levels updated ({_light.color.b:0.00}/1)");
             }
-            foreach (var renderer in renderers)
-            {
-                foreach (var material in renderer.materials)
-                {
-                    material.SetColor(ShaderPropertyID._GlowColor, new Color(light.color.r, light.color.g, light.color.b));
-                }
-            }
-            reticle.SetInteractText($"Press \"R\" to change the Red Levels, current red Level: {light.color.r:0.00}\nPress \"G\" to change the Green Levels, current Green Level: {light.color.g:0.00}\nPress \"B\" to change the Blue Levels, current Blue level: {light.color.b:0.00}");
+            
+            reticle.SetInteractText($"Press \"R\" to change the Red Levels, current red Level: {_light.color.r:0.00}\nPress \"G\" to change the Green Levels, current Green Level: {_light.color.g:0.00}\nPress \"B\" to change the Blue Levels, current Blue level: {_light.color.b:0.00}");
         }
         public void OnProtoSerialize(ProtobufSerializer serializer)
         {
@@ -148,7 +149,7 @@ namespace ColorizableSpotlight.MonoBehaviours
                 {
                     writer.Write(JsonConvert.SerializeObject(colorInfo, Formatting.Indented));
                 }
-                savedColor = new Color(colorInfo.RedLevel, colorInfo.GreenLevel, colorInfo.BlueLevel);
+                _savedColor = new Color(colorInfo.RedLevel, colorInfo.GreenLevel, colorInfo.BlueLevel);
             }
         }
 
--- ColorizableSpotlight/Patches/BaseSpotlight_Start_Patch.cs
+++ ColorizableSpotlight/Patches/BaseSpotlight_Start_Patch.cs
@@ -1,16 +1,18 @@
-﻿using HarmonyLib;
-using ColorizableSpotlight.MonoBehaviours;
-namespace ColorizableSpotlight.Patches
+﻿namespace ColorizableSpotlight.Patches
 {
+    using HarmonyLib;
+    using MonoBehaviours;
+    
     [HarmonyPatch(typeof(BaseSpotLight), nameof(BaseSpotLight.Start))]
     public static class BaseSpotlight_Start_Patch
     {
         public static void Postfix(BaseSpotLight __instance)
         {
-            TechType techType = CraftData.GetTechType(__instance.gameObject);
+            var techType = CraftData.GetTechType(__instance.gameObject);
             if (techType == TechType.Spotlight)
             {
-                __instance.gameObject.EnsureComponent<ColoringController>();
+                var cc = __instance.gameObject.EnsureComponent<ColoringController>();
+                cc.enabled = __instance.constructed;
             }
         }
     }
