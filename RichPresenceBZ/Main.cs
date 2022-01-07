44fec5c06578fb8de4ccfdb3e4857d4de47aeff3
Metious <71298690+Metious@users.noreply.github.com>
2022-01-07 12:28:28 +0330

    Suppress null warning

--- RichPresenceBZ/Main.cs
+++ RichPresenceBZ/Main.cs
@@ -2,15 +2,13 @@
 using System.Reflection;
 using QModManager.API.ModLoading;
 using RichPresenceBZ.MonoBehaviours;
-using UnityEngine;
-using UnityEngine.SceneManagement;
 
 namespace RichPresenceBZ
 {
     [QModCore]
     public static class Main
     {
-        public static string AssetsFolder { get; } = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Assets");
+        public static string AssetsFolder { get; } = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "Assets");
         
         [QModPatch]
         public static void Load() => DiscordRPManager.Initialize();
