7f2ef412d262a382d315c9e2dbdd06839e8c9438
Metious <71298690+Metious@users.noreply.github.com>
2022-01-07 12:23:07 +0330

    Version bump

--- ColorizableSpotlight/Main.cs
+++ ColorizableSpotlight/Main.cs
@@ -12,7 +12,7 @@ namespace ColorizableSpotlight
         public static string Combine(string path1, string path2) => Path.Combine(path1, path2).Replace('\\', '/');
         public static string GetSaveFolderPath() => Path.GetFullPath(Path.Combine(ModPath, "../../SNAppData/SavedGames/", SaveLoadManager.main.GetCurrentSlot(), "ColorizedSpotlight")).Replace('\\', '/');
         
-        public const string version = "1.0.5.0";
+        public const string version = "1.0.6.0";
         [QModPatch]
         public static void Load()
         {
